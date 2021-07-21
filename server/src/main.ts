49842ec37e1d3cae65242d6a477fa6deceeac519
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-07-22 06:35:51 +0800

    Fix Linting

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { config } from 'dotenv'
-import { Console } from 'console'
 
 config()
 async function bootstrap() {
